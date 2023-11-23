51c9094fd0bd6352e62ffed95e4081b09a1be133
hakonwb <119631934+hakonwb@users.noreply.github.com>
2023-11-22 18:18:19 -0600

    Create processm.cpp

---
+++ processm.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class ProcessControlBlock {
+public:
+    int processID;
+    std::string state;
+    int programCounter;
+    // Aquí puedes agregar más atributos como uso de memoria, tiempos, etc.
+
+    ProcessControlBlock(int id) : processID(id), state("new"), programCounter(0) {}
+
+    void displayInfo() {
+        std::cout << "Proceso ID: " << processID << "\n";
+        std::cout << "Estado: " << state << "\n";
+        std::cout << "Contador de Programa: " << programCounter << "\n\n";
+    }
+};
+
+class Scheduler {
+private:
+    std::vector<ProcessControlBlock> queue;
+
+public:
+    void addProcess(ProcessControlBlock pcb) {
+        queue.push_back(pcb);
+    }
+
+    void displayQueue() {
+        for (auto& pcb : queue) {
+            pcb.displayInfo();
+        }
+    }
+};
+
+int main() {
+    Scheduler scheduler;
+
+    // Creando y añadiendo procesos
+    ProcessControlBlock pcb1(1);
+    ProcessControlBlock pcb2(2);
+    ProcessControlBlock pcb3(3);
+
+    scheduler.addProcess(pcb1);
+    scheduler.addProcess(pcb2);
+    scheduler.addProcess(pcb3);
+
+    // Mostrando la cola del scheduler
+    scheduler.displayQueue();
+
+    return 0;
+}
